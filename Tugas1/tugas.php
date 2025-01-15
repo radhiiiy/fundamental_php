f9a6d67bfc2a5857b5c741be677b9fdc821442d8
radhiiiy <radhiyyan232@gmail.com>
2025-01-15 14:15:03 +0700

    add

---
+++ Tugas1/tugas.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="" /> -->
+    <title>Document</title>
+</head>
+<body>
+    <H3>
+      <?php 
+
+define(constant_name: "BONUS", value: 500000);
+define(constant_name: "PPN", value: 0.05);
+define(constant_name: "GAJI", value: 5000000);
+
+$GajiBersih = 0;
+$TotalGaji = 0;
+$JmlPryk = 1;
+
+
+echo "Gaji anda adalah : " . GAJI;
+
+
+
+
+?>
+    </H3>
+</body>
+</html>
+
+
+
+
